9218daf84bb096505845d5ac0018a0e59308fe1d
CrazedPotato <115425897+CrazedPotato@users.noreply.github.com>
2022-10-27 19:15:27 -0700

    Update c420690075.lua

--- script/c420690075.lua
+++ script/c420690075.lua
@@ -11,7 +11,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.count(c)
-	return Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsRace,RACE_WINGEDBEAST),c:GetControler(),LOCATION_MZONE,0,nil)
+	return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsRace,RACE_WINGEDBEAST),c:GetControler(),LOCATION_MZONE,0,nil)
 end
 function s.filter(c,e,tp)
 	return c:GetLevel() <= s.count(c) and c:IsRace(RACE_WINGEDBEAST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
