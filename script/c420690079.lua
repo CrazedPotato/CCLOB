b39f970a18ec1a6472a3ae20d50832da45ce8f67
CrazedPotato <115425897+CrazedPotato@users.noreply.github.com>
2022-10-27 19:16:28 -0700

    Update c420690079.lua

--- script/c420690079.lua
+++ script/c420690079.lua
@@ -14,5 +14,5 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function s.atkup(e,c)
-	return Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsRace,RACE_DRAGON),c:GetControler(),LOCATION_MZONE,0,nil)*500
+	return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsRace,RACE_DRAGON),c:GetControler(),LOCATION_MZONE,0,nil)*500
 end
