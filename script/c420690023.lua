a690776a30a70f31996ce790b3f9da871893557c
CrazedPotato <115425897+CrazedPotato@users.noreply.github.com>
2022-10-27 19:05:13 -0700

    Update c420690023.lua

--- script/c420690023.lua
+++ script/c420690023.lua
@@ -33,5 +33,5 @@ end
 function s.condition(e,c)
 	if c==nil then return true end
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-		and	Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_FIEND),c:GetControler(),LOCATION_MZONE,0,1,nil)
+		and	Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_FIEND),c:GetControler(),LOCATION_MZONE,0,1,nil)
 end
